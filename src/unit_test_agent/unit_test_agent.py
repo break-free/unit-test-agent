f19b8b615f98e2b3959fe71b5fdd0287dd4ca27c
Chris Mills <90063465+cmills-bf@users.noreply.github.com>
2023-08-01 13:25:46 +0000

    Fix bam529 refactoring (#2)
    * refactored code removing some of the hard-coded values
    * renamed main to demo

---
+++ src/unit_test_agent/unit_test_agent.py
@@ -0,0 +1,41 @@
+from src.unit_test_agent.chains import CreateUnitTest
+from src.unit_test_agent.indexer import ConfirmVectorstoreCollectionIsEmpty, GetOrCreateVectorstore, SimilaritySearchVectorstore
+from langchain.agents import initialize_agent, AgentType
+from langchain.agents.agent import AgentExecutor
+from langchain.schema import BaseMemory
+from langchain.schema.language_model import BaseLanguageModel
+from src.unit_test_agent.tools import DummyTestCoverage, SaveToNewFile, RunTestSuiteTool
+
+
+class UnitTestAgent():
+
+    llm: BaseLanguageModel
+    conversational_memory: BaseMemory
+    agent: AgentExecutor
+
+    def __init__(self,
+                 llm: BaseLanguageModel,
+                 conversational_memory: BaseMemory):
+        self.llm = llm
+        self.conversational_memory = conversational_memory
+
+        agent_tools = [DummyTestCoverage(),
+                       ConfirmVectorstoreCollectionIsEmpty(),
+                       GetOrCreateVectorstore(),
+                       SimilaritySearchVectorstore(),
+                       CreateUnitTest(llm),
+                       SaveToNewFile(),
+                       RunTestSuiteTool()]
+
+        # initialize agent with tools
+        self.agent = initialize_agent(
+            agent=AgentType.STRUCTURED_CHAT_ZERO_SHOT_REACT_DESCRIPTION,
+            tools=agent_tools,
+            llm=llm,
+            verbose=True,
+            memory=conversational_memory,
+            return_intermediate_step=True
+        )
+
+    def run_agent(self, prompt: str):
+        self.agent(prompt)
