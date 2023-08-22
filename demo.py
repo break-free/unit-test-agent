6fe5e2ca38365b0a0983eae67ef6769bd369a014
Chris Mills <cmills@breakfreesolutions.com>
2023-08-22 11:01:44 -0500

    reduced token limit for token buffer memory

--- demo.py
+++ demo.py
@@ -35,7 +35,7 @@ if __name__ == "__main__":
     # initialize conversational memory using tokens
     conversational_memory = ConversationTokenBufferMemory(
         llm=llm,
-        max_token_limit=3500,
+        max_token_limit=3400,
         return_messages=True,
         verbose=True
     )
