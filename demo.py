ff78ae0ec316a4ecce259456b74851fff7cab2b1
Chris Mills <cmills@breakfreesolutions.com>
2023-08-22 09:55:20 -0500

    updated model and memory token limit

--- demo.py
+++ demo.py
@@ -28,14 +28,14 @@ if __name__ == "__main__":
     llm = ChatOpenAI(
         openai_api_key=os.environ['OPENAI_API_KEY'],
         temperature=0,
-        model_name='gpt-4',
+        model_name='gpt-4-32k',
         max_retries=20
     )
 
     # initialize conversational memory using tokens
     conversational_memory = ConversationTokenBufferMemory(
         llm=llm,
-        max_token_limit=3750,
+        max_token_limit=15000,
         return_messages=True,
         verbose=True
     )
