89684437d3cf6fb6fcc5c5ae848f28e6d95ee799
Chris Mills <90063465+cmills-bf@users.noreply.github.com>
2023-08-24 21:07:37 +0000

    Refactoring for demo (#9)
    * Updated README.adoc
    * Refactor demo.py to use formatting and reduce unused prompts
    * Updated demo.py so the default number of unit tests generated is one.

--- demo.py
+++ demo.py
@@ -9,6 +9,18 @@ from src.unit_test_agent.unit_test_agent import UnitTestAgent
 import sys
 import time
 
+
+class bcolors:
+    BOLD = '\033[1m'
+    RED = '\033[91m'
+    GREEN = '\033[92m'
+    YELLOW = '\033[93m'
+    BLUE = '\033[94m'
+    MAGENTA = '\033[95m'
+    CYAN = '\033[96m'
+    END = '\033[0m'
+
+
 if __name__ == "__main__":
 
     # Check that environment variables are set up.
@@ -41,37 +53,51 @@ if __name__ == "__main__":
         verbose=True
     )
 
-    agent = UnitTestAgent(llm, conversational_memory)
-
+    # Create dummy code coverage output for agent to create tests
     returned_tests = [
         '{ file: "fineract/fineract-client/src/main/java/org/apache/fineract/client/util/Parts.java", class: "Parts", method: "fromFile" }'
-        # '{ file: "fineract/fineract-client/src/main/java/org/apache/fineract/client/util/Calls.java", class: "Calls", method: "ok" }',
-        # '[ file: "fineract/fineract-client/src/main/java/org/apache/fineract/client/util/CallFailedRuntimeException.java", class: "CallFailedRuntimeException", method: "message" ]'
+        # '{ file: "fineract/fineract-client/src/main/java/org/apache/fineract/client/util/Calls.java", class: "Calls", method: "ok" }'
     ]
 
-    with get_openai_callback() as cb:
+    # Finally create, the Agent
+    agent = UnitTestAgent(llm, conversational_memory)
 
-        all_agent_total_tokens = 0
-        all_agent_prompt_tokens = 0
-        all_agent_completion_tokens = 0
-        all_agent_total_cost = 0
+    # Variables for statistics
+    start_all = time.time()
+    all_agent_total_tokens = 0
+    all_agent_prompt_tokens = 0
+    all_agent_completion_tokens = 0
+    all_agent_total_cost = 0
 
-        start_all = time.time()
+    for test in returned_tests:
 
-        for test in returned_tests:
+        # Callback allows us to capture data about tokens used and expenditure
+        with get_openai_callback() as cb:
 
             # Create a unit test and then attempt to fix the errors #
-            prompt = f"Create a test class for this class and method: `{test}`. Use the local vector store to retrieve information on the method, class and package. If the vector store is empty, populate the vector store with code from the following directory '{args.data_path}'. Once created the test class should be saved to disk using an appropriate file name within `{args.data_path}`, and then tested. If there are any errors then attempt to fix them until resolved."
+            prompt = (
+                f"""Create a test class for this class and method: `{test}`. Use the local """
+                f"""vector store to retrieve information on the method, class and package. If """
+                f"""the vector store is empty, populate the vector store with code from the """
+                f"""following directory '{args.data_path}'. Once created the test class should """
+                f"""be saved to disk using an appropriate file name within `{args.data_path}`, """
+                f"""and then tested. If there are any errors then attempt to fix them until """
+                f"""resolved."""
+            )
 
+            # Run the agent and capture time statistics
             start_agent = time.time()
             agent.run_agent(prompt)
             end_agent = time.time()
-            print(f"Agent Execution took: {end_agent - start_agent}")
+
+            print(
+                bcolors.BOLD + bcolors.RED + f"\nAgent Execution took: {end_agent - start_agent}"
+            )
             print(
                 f"""Agent Total Tokens: {cb.total_tokens}, """
                 f"""Prompt Tokens: {cb.prompt_tokens}, """
                 f"""Completion Tokens: {cb.completion_tokens}\n """
-                f"""Total Cost (USD): ${cb.total_cost}"""
+                f"""Total Cost (USD): ${cb.total_cost}""" + bcolors.END
             )
 
             all_agent_total_tokens += cb.total_tokens
@@ -79,21 +105,12 @@ if __name__ == "__main__":
             all_agent_completion_tokens += cb.completion_tokens
             all_agent_total_cost += cb.total_cost
 
-        end_all = time.time()
-        print(f"All Agent Execution took: {end_all - start_all} seconds.")
-        print(
-            f"""All Agents -> Total Tokens: {all_agent_total_tokens}, """
-            f"""Prompt Tokens: {all_agent_prompt_tokens}, """
-            f"""Completion Tokens: {all_agent_completion_tokens}\n """
-            f"""All Agents -> Total Cost (USD): ${all_agent_total_cost}"""
-        )
+    end_all = time.time()
 
-    # A human in the loop prompt
-    # prompt = f"Create one test class as needed for each method reported by the test coverage tool. Use the local vector store to retrieve information on the method, its class and its package as often as needed. If the vector store is empty, populate the vector store with code from the following directory '{args.data_path}'. Once created, the test class should be saved to disk using an appropriate file name, checked by a human, and then tested. If there are any errors then attempt to fix them with human input. "
-
-    # Create a unit test but do not attempt to fix the errors #
-    # prompt = f"Create one test class as needed for each method reported by the test coverage tool. Use the local vector store to retrieve information on the method, its class and its package. If the vector store is empty, populate the vector store with code from the following directory '{args.data_path}'. Once created the test class should be saved to disk using an appropriate file name within `{args.data_path}`, and then tested. If there are any errors then explain them but do not attempt to fix them."
-
-    # Test prompts for running specific tools! ##
-    # prompt = "Run the test suite tool and report errors but do not fix them."
-    # prompt = "Return classes and methods that require additional testing."
+    print(bcolors.BOLD + bcolors.RED + f"All Execution took: {end_all - start_all} seconds.")
+    print(
+        f"""All Agents -> Total Tokens: {all_agent_total_tokens}, """
+        f"""Prompt Tokens: {all_agent_prompt_tokens}, """
+        f"""Completion Tokens: {all_agent_completion_tokens}\n """
+        f"""All Agents -> Total Cost (USD): ${all_agent_total_cost}""" + bcolors.END
+    )
