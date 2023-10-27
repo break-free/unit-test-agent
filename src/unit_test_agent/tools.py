a62f8d75bf1596ead68132cd3f8c187a52c2420a
Chris Mills <cmills@breakfreesolutions.com>
2023-10-27 13:29:46 -0500

    correct filter word

--- src/unit_test_agent/tools.py
+++ src/unit_test_agent/tools.py
@@ -133,7 +133,7 @@ class RunTestSuiteTool(BaseTool):
             filter_words = ['cucumber.core', '0 Scenarios', '0 Steps', '0m0.', '--EclipseLink',
                             'BusinessDate', 'interfaceClass', '<T>getBean(Class<T>)', 'found: ',
                             'required: ', 'where T is a type-variable', 'warning', 'WARNING',
-                            'DEBUG', '* ', '> ', '^', 'request.getBody() != null']
+                            'DEBUG', '* ', '\n> ', '^', 'request.getBody() != null']
             return_string = filter_words_whitespace(process.stderr, filter_words) + "\n"
             return_string = return_string + filter_words_whitespace(process.stdout, filter_words)
             return "Errors were encountered:\n" + return_string
